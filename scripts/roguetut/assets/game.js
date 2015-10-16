4395b637ead35ad0b148bed26b596365c02c12ea
Stephen Rumpel <jimbobmaggot@gmail.com>
2015-10-16 13:19:48 -0400

    Update game.js

--- scripts/roguetut/assets/game.js
+++ scripts/roguetut/assets/game.js
@@ -4,32 +4,47 @@
 var Game =  {
 	_display: null,
 	_currentScreen: null,
+    _screenWidth: 80,
+    _screenHeight: 24,
 	init: function () {
         'use strict';
-	    // Any necessary initialization will go here.
-	    this._display = new ROT.Display({width: 80, height: 24});
-	    // Create a helper function for binding to an event
-	    // and making it send it to the screen
-	    var game = this, // So that we don't lose this
+        // Any necessary initialization will go here.
+        this._display = new ROT.Display({width: this._screenWidth,
+                                         height: this._screenHeight});
+        // Create a helper function for binding to an event
+        // and making it send it to the screen
+        var game = this, // So that we don't lose this
             bindEventToScreen = function (event) {
                 window.addEventListener(event, function (e) {
-	                // When an event is received, send it to the
-	                // screen if there is one
-	                if (game._currentScreen !== null) {
-	                    // Send the event type and data to the screen
-	                    game._currentScreen.handleInput(event, e);
-	                }
-	            });
-	        };
+                // When an event is received, send it to the
+                // screen if there is one
+                    if (game._currentScreen !== null) {
+                        // Send the event type and data to the screen
+                        game._currentScreen.handleInput(event, e);
+                        // Clear the screen
+                        game._display.clear();
+                        // Render the screen
+                        game._currentScreen.render(game._display);
+                    }
+                });
+            };
         // Bind keyboard input events
         bindEventToScreen('keydown');
-        bindEventToScreen('keyup');
-        bindEventToScreen('keypress');
-	},
+        //bindEventToScreen('keyup');
+        //bindEventToScreen('keypress');
+    },
 	getDisplay: function () {
         'use strict';
 		return this._display;
 	},
+    getScreenWidth: function () {
+        'use strict';
+        return this._screenWidth;
+    },
+    getScreenHeight: function () {
+        'use strict';
+        return this._screenHeight;
+    },
 	switchScreen: function (screen) {
         'use strict';
 	    // If we had a screen before, notify it that we exited
