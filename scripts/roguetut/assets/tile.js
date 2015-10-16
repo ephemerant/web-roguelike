1e5ffc00d4f3c2372811f935b3d1e70c6eeb4161
Stephen Rumpel <jimbobmaggot@gmail.com>
2015-10-16 15:13:03 -0400

    Update tile.js

--- scripts/roguetut/assets/tile.js
+++ scripts/roguetut/assets/tile.js
@@ -1,16 +1,35 @@
 /*global Game */
 /*jslint nomen: true*/
 
-Game.Tile = function (glyph) {
+Game.Tile = function (properties) {
     'use strict';
-    this._glyph = glyph;
+    properties = properties || {};
+    // Call the Glyph constructor with our properties
+    Game.Glyph.call(this, properties);
+    // Set up the properties. We use false by default.
+    this._isWalkable = properties.isWalkable || false;
+    //this._isDiggable = properties.isDiggable || false;
+};
+// Make tiles inherit all the functionality from glyphs
+Game.Tile.extend(Game.Glyph);
+
+// Standard getters
+Game.Tile.prototype.isWalkable = function () {
+    'use strict';
+    return this._isWalkable;
+};
+Game.Tile.prototype.isDiggable = function () {
+    'use strict';
+    return this._isDiggable;
 };
 
-Game.Tile.prototype.getGlyph = function () {
-    'use strict';
-    return this._glyph;
-};
-
-Game.Tile.nullTile = new Game.Tile(new Game.Glyph());
-Game.Tile.floorTile = new Game.Tile(new Game.Glyph('.'));
-Game.Tile.wallTile = new Game.Tile(new Game.Glyph('#', 'goldenrod'));
+Game.Tile.nullTile = new Game.Tile({});
+Game.Tile.floorTile = new Game.Tile({
+    character: '.',
+    isWalkable: true
+});
+Game.Tile.wallTile = new Game.Tile({
+    character: '#',
+    foreground: 'goldenrod',
+    isWalkable: false
+});
