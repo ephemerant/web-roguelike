eaa1e68db1c59aea4342044895b78ec2ded3841d
ephemerant <clayton.mcg@gmail.com>
2015-10-18 02:15:57 -0400

    now using require.js

---
+++ js/app/dungeon.js
@@ -0,0 +1,248 @@
+define(['ROT', 'lodash'], function(ROT, _) {
+
+  var TILESHEET_WIDTH = 640;
+  var TILE_SIZE = 32;
+
+  var TILE_UNIT = TILESHEET_WIDTH / TILE_SIZE;
+
+  var WALL_GROUP_UNIT = TILE_UNIT * 3;
+
+  // Use the 4th set of walls going down (blue stone)
+  WALL_GROUP_UNIT *= 3;
+
+  // List of tiles and their corresponding position in the tile sheet
+  var tiles = {
+    floor: 15,
+    door: 30,
+    stairs: 34,
+    wall_vertical: 20 + WALL_GROUP_UNIT,
+    wall_horizontal: 1 + WALL_GROUP_UNIT,
+    // Corner pieces
+    wall_top_right: 2 + WALL_GROUP_UNIT,
+    wall_bottom_right: 42 + WALL_GROUP_UNIT,
+    wall_top_left: 0 + WALL_GROUP_UNIT,
+    wall_bottom_left: 40 + WALL_GROUP_UNIT,
+    // Cross pieces
+    wall_cross_bottom: 44 + WALL_GROUP_UNIT,
+    wall_cross_top: 4 + WALL_GROUP_UNIT,
+    wall_cross_left: 23 + WALL_GROUP_UNIT,
+    wall_cross_right: 25 + WALL_GROUP_UNIT,
+    wall_cross: 24 + WALL_GROUP_UNIT
+  };
+
+  var crosses = [tiles.wall_cross_bottom, tiles.wall_cross_top, tiles.wall_cross_left, tiles.wall_cross_right, tiles.wall_cross];
+
+  var dungeon = {
+
+    width: 60,
+    height: 40,
+
+    level: 1,
+
+    _init: function() {
+      ROT.RNG.setSeed(ROT.RNG.getUniform());
+
+      console.log('Using RNG seed:', ROT.RNG.getSeed());
+
+      this.tiles = {};
+
+      this._generate();
+
+      this._placeDoors();
+
+      this._placeWalls();
+
+      var keys = Object.keys(dungeon.tiles);
+
+      var tile = keys[0].split(',');
+
+      this.player = {
+        x: +tile[0],
+        y: +tile[1]
+      };
+
+      tile = keys[keys.length - 1].split(',');
+
+      this.stairs = {
+        x: +tile[0],
+        y: +tile[1]
+      };
+
+    },
+
+    _generate: function() {
+      var digger = new ROT.Map.Digger(this.width, this.height);
+
+      var digCallback = function(x, y, value) {
+        if (value) {
+          return;
+        }
+        this.tiles[(x) + ',' + (y)] = tiles.floor;
+      };
+
+      digger.create(digCallback.bind(this));
+
+      this.digger = digger;
+    },
+
+    // Analyze rooms and place doors
+    _placeDoors: function() {
+
+      var doors = [];
+
+      _.each(this.digger.getRooms(), function(room, key) {
+        _.each(room._doors, function(door, key) {
+          var xy = key.split(',');
+          var x = +xy[0],
+            y = +xy[1];
+          // Don't place doors next to each other
+          if (_.contains(doors, (x + 1) + ',' + (y)) || _.contains(doors, (x) + ',' + (y + 1)) || _.contains(doors, (x - 1) + ',' + (y)) ||
+            _.contains(doors, (x) + ',' + (y - 1))) {
+
+          }
+          // Avoid duplicates
+          else if (!_.contains(doors, key)) {
+            // 60% chance to spawn a door
+            if (ROT.RNG.getPercentage() <= 60) {
+              doors.push(key);
+            }
+          }
+        });
+      });
+
+      this.doors = doors;
+    },
+
+    // Place and autojoin walls around tiles
+    _placeWalls: function() {
+
+      var walls = {};
+
+      var myTiles = this.tiles;
+      var doors = this.doors;
+
+      // Place unjoined walls
+      _.each(myTiles, function(tile, key) {
+
+        var xy = key.split(',');
+
+        var x = +(xy[0]),
+          y = +(xy[1]);
+
+        var left = (x - 1) + ',' + (y),
+          right = (x + 1) + ',' + (y),
+          up = (x) + ',' + (y - 1),
+          down = (x) + ',' + (y + 1),
+          top_right = (x + 1) + ',' + (y - 1),
+          bottom_right = (x + 1) + ',' + (y + 1),
+          bottom_left = (x - 1) + ',' + (y + 1),
+          top_left = (x - 1) + ',' + (y - 1);
+
+        var directions = [left, right, up, down, top_right, bottom_right, bottom_left, top_left];
+
+        _.each(directions, function(direction) {
+          if (myTiles[direction] === undefined && walls[direction] === undefined) {
+            walls[direction] = true;
+          }
+        });
+      });
+
+      // Join walls
+      _.each(walls, function(tile, key) {
+
+        var xy = key.split(',');
+
+        var x = +(xy[0]),
+          y = +(xy[1]);
+
+        var left = (x - 1) + ',' + (y),
+          right = (x + 1) + ',' + (y),
+          up = (x) + ',' + (y - 1),
+          down = (x) + ',' + (y + 1);
+
+        var directions = [left, right, up, down];
+
+        var nearbyWalls = [];
+
+        // Create list of surrounding walls based on direction
+        _.each(directions, function(direction) {
+          if (walls[direction] !== undefined || _.contains(doors, direction)) {
+            nearbyWalls.push(direction);
+          }
+        });
+
+        // Crosses
+        if (_.contains(nearbyWalls, left) && _.contains(nearbyWalls, right) && _.contains(nearbyWalls, up) && _.contains(nearbyWalls, down)) {
+          walls[key] = tiles.wall_cross;
+        } else if (_.contains(nearbyWalls, left) && _.contains(nearbyWalls, right) && _.contains(nearbyWalls, up)) {
+          walls[key] = tiles.wall_cross_bottom;
+        } else if (_.contains(nearbyWalls, left) && _.contains(nearbyWalls, right) && _.contains(nearbyWalls, down)) {
+          walls[key] = tiles.wall_cross_top;
+        } else if (_.contains(nearbyWalls, down) && _.contains(nearbyWalls, right) && _.contains(nearbyWalls, up)) {
+          walls[key] = tiles.wall_cross_left;
+        } else if (_.contains(nearbyWalls, left) && _.contains(nearbyWalls, up) && _.contains(nearbyWalls, down)) {
+          walls[key] = tiles.wall_cross_right;
+        }
+        // Corners
+        else if (_.contains(nearbyWalls, down) && _.contains(nearbyWalls, right)) {
+          walls[key] = tiles.wall_top_left;
+        } else if (_.contains(nearbyWalls, down) && _.contains(nearbyWalls, left)) {
+          walls[key] = tiles.wall_top_right;
+        } else if (_.contains(nearbyWalls, up) && _.contains(nearbyWalls, right)) {
+          walls[key] = tiles.wall_bottom_left;
+        } else if (_.contains(nearbyWalls, up) && _.contains(nearbyWalls, left)) {
+          walls[key] = tiles.wall_bottom_right;
+        }
+        // Horizontal / Vertical
+        else if (_.contains(nearbyWalls, up) || _.contains(nearbyWalls, down)) {
+          walls[key] = tiles.wall_vertical;
+        } else if (_.contains(nearbyWalls, left) || _.contains(nearbyWalls, right)) {
+          walls[key] = tiles.wall_horizontal;
+        }
+      });
+
+      // Smooth out cross stacking (optional)
+      _.each(walls, function(tile, key) {
+        if (_.contains(crosses, tile)) {
+          var xy = key.split(',');
+
+          var x = +(xy[0]),
+            y = +(xy[1]);
+
+          var left = (x - 1) + ',' + (y),
+            up = (x) + ',' + (y - 1);
+
+          // Turn cross pairs into horizontal/vertical
+          if (tile === tiles.wall_cross_bottom && walls[up] === tiles.wall_cross_top) {
+            walls[key] = tiles.wall_horizontal;
+            walls[up] = tiles.wall_horizontal;
+          } else if (tile === tiles.wall_cross_right && walls[left] === tiles.wall_cross_left) {
+            walls[key] = tiles.wall_vertical;
+            walls[left] = tiles.wall_vertical;
+          }
+          // Decompose full crosses
+          else if (tile === tiles.wall_cross_right && walls[left] === tiles.wall_cross) {
+            walls[key] = tiles.wall_vertical;
+            walls[left] = tiles.wall_cross_right;
+          } else if (tile === tiles.wall_cross_bottom && walls[up] === tiles.wall_cross) {
+            walls[key] = tiles.wall_horizontal;
+            walls[up] = tiles.wall_cross_bottom;
+          } else if (tile === tiles.wall_cross && walls[left] === tiles.wall_cross_left) {
+            walls[key] = tiles.wall_cross_left;
+            walls[left] = tiles.wall_vertical;
+          } else if (tile === tiles.wall_cross && walls[up] === tiles.wall_cross_top) {
+            walls[key] = tiles.wall_cross_top;
+            walls[up] = tiles.wall_horizontal;
+          }
+        }
+      });
+      this.walls = walls;
+    }
+  };
+
+  return {
+    TILE_SIZE: TILE_SIZE,
+    tiles: tiles,
+    dungeon: dungeon
+  };
+});
---
+++ js/app/game.js
@@ -0,0 +1,257 @@
+define(['Phaser', 'lodash', 'dungeon', 'ROT'], function(Phaser, _, dungeon, ROT) {
+
+  var tiles = dungeon.tiles;
+
+  var TILE_SIZE = dungeon.TILE_SIZE;
+
+  dungeon = dungeon.dungeon;
+
+  // get dimensions of the window considering retina displays
+  var SCREEN_WIDTH = window.innerWidth * window.devicePixelRatio;
+  var SCREEN_HEIGHT = window.innerHeight * window.devicePixelRatio;
+
+  var DUNGEON_WIDTH = dungeon.width * TILE_SIZE;
+  var DUNGEON_HEIGHT = dungeon.width * TILE_SIZE;
+
+  var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'screen', {
+    preload: preload,
+    create: create,
+    update: update,
+    render: render
+  });
+
+  function preload() {
+
+    game.load.image('dungeon', 'assets/Wall.png');
+    game.load.spritesheet('warrior', 'assets/Warrior.png', TILE_SIZE, TILE_SIZE);
+    game.load.spritesheet('engineer', 'assets/Engineer.png', TILE_SIZE, TILE_SIZE);
+    game.load.spritesheet('mage', 'assets/Mage.png', TILE_SIZE, TILE_SIZE);
+    game.load.spritesheet('paladin', 'assets/Paladin.png', TILE_SIZE, TILE_SIZE);
+    game.load.spritesheet('rogue', 'assets/Rogue.png', TILE_SIZE, TILE_SIZE);
+
+  }
+
+  var map;
+  var layer;
+
+  var cursors;
+  var reset_key;
+  var autopilot_key;
+
+  var player;
+
+  function create() {
+
+
+    // // Zoom camera in
+    game.camera.scale.set(1);
+    // // Increase bounds so camera can move around
+    game.world.setBounds(-DUNGEON_WIDTH, -DUNGEON_HEIGHT, DUNGEON_WIDTH * 3, DUNGEON_HEIGHT * 3);
+
+    game.stage.backgroundColor = '#050505';
+
+    game.stage.smoothed = false;
+
+    // Creates a blank tilemap
+    map = game.add.tilemap(null, TILE_SIZE, TILE_SIZE);
+
+    // Add a Tileset image to the map
+    map.addTilesetImage('dungeon');
+
+    // Creates a new blank layer and sets the map dimensions.
+    layer = map.create('level1', dungeon.width, dungeon.height, TILE_SIZE, TILE_SIZE);
+
+    createWorld();
+
+    cursors = game.input.keyboard.createCursorKeys();
+
+    autopilot_key = game.input.keyboard.addKey(Phaser.Keyboard.A);
+
+    reset_key = game.input.keyboard.addKey(Phaser.Keyboard.R);
+    reset_key.onDown.add(createWorld, this);
+
+  }
+
+  function autoPilot() {
+
+    if (player.isMoving) return;
+
+    // Input callback informs about map structure
+    var passableCallback = function(x, y) {
+      return (dungeon.tiles[x + "," + y] !== undefined);
+    }
+
+    // Prepare path to stairs
+    var astar = new ROT.Path.AStar(dungeon.stairs.x, dungeon.stairs.y, passableCallback, {
+      topology: 4
+    });
+
+    var exit = 0;
+
+    // Compute from player
+    astar.compute(dungeon.player.x, dungeon.player.y, function(x, y) {
+
+      exit += 1;
+      // Only move once
+      if (exit !== 2) return;
+
+      var _x = x - dungeon.player.x,
+        _y = y - dungeon.player.y;
+
+      movePlayer(_x, _y);
+    })
+  }
+
+  function createWorld() {
+    dungeon.level = 1;
+    createDungeon();
+    createPlayer();
+  }
+
+  function createDungeon() {
+    removeTiles();
+
+    dungeon._init();
+
+    // Place tiles
+    _.each(dungeon.tiles, function(tile, key) {
+      var xy = key.split(',');
+      placeTile(tile, xy[0], xy[1]);
+    });
+
+    // Place walls
+    _.each(dungeon.walls, function(tile, key) {
+      var xy = key.split(',');
+      placeTile(tile, xy[0], xy[1]);
+    });
+
+    // Place doors
+    _.each(dungeon.doors, function(key) {
+      var xy = key.split(',');
+      placeTile(tiles.door, xy[0], xy[1]);
+    });
+
+    // Place stairs
+    placeTile(tiles.stairs, dungeon.stairs.x, dungeon.stairs.y);
+  }
+
+  function createPlayer() {
+    if (player) {
+      player.destroy();
+    }
+
+    var playerClass = ['warrior', 'engineer', 'mage', 'paladin', 'rogue'][_.random(4)];
+
+    player = game.add.sprite(0, 0, playerClass, 0);
+    player.animations.add('left', [4, 5, 6, 7], 10, true);
+    player.animations.add('right', [8, 9, 10, 11], 10, true);
+    player.animations.add('up', [12, 13, 14, 15], 10, true);
+    player.animations.add('down', [0, 1, 2, 3], 10, true);
+    game.camera.follow(player);
+
+    player.x = dungeon.player.x * TILE_SIZE;
+    player.y = dungeon.player.y * TILE_SIZE;
+  }
+
+  function placeTile(tile, x, y) {
+    map.putTile(tile, x, y, layer);
+  }
+
+  function removeTiles() {
+    // Tiles
+    _.each(dungeon.tiles, function(tile, key) {
+      var xy = key.split(',');
+      map.removeTile(xy[0], xy[1], layer);
+    });
+
+    // Walls
+    _.each(dungeon.walls, function(tile, key) {
+      var xy = key.split(',');
+      map.removeTile(xy[0], xy[1], layer);
+    });
+
+    // Doors
+    _.each(dungeon.doors, function(key) {
+      var xy = key.split(',');
+      map.removeTile(xy[0], xy[1], layer);
+    });
+  }
+
+  function movePlayer(x, y) {
+
+    if (player.isMoving) return;
+
+    if (x === 0 && y === 0) return;
+
+    var key = (dungeon.player.x + x) + ',' + (dungeon.player.y + y);
+
+    if (x === 1) {
+      player.play('right');
+    } else if (x === -1) {
+      player.play('left');
+    }
+    if (y === 1) {
+      player.play('down');
+    } else if (y === -1) {
+      player.play('up');
+    }
+
+    // Valid tile
+    if (dungeon.tiles[key] !== undefined) {
+
+      player.isMoving = true;
+
+      dungeon.player.x += x;
+      dungeon.player.y += y;
+
+      // Entering stairs
+      if (dungeon.player.x === dungeon.stairs.x && dungeon.player.y === dungeon.stairs.y) {
+        dungeon.level += 1;
+        createDungeon();
+      }
+
+      // Slide the player to their new position
+      game.add.tween(player).to({
+        x: dungeon.player.x * TILE_SIZE,
+        y: dungeon.player.y * TILE_SIZE
+      }, 80, Phaser.Easing.Quadratic.InOut, true).onComplete.add(function() {
+        player.isMoving = false;
+      }, this);
+    }
+
+  }
+
+  function update() {
+
+    if (cursors.left.isDown) {
+      movePlayer(-1, 0);
+      player.play('left');
+    } else if (cursors.right.isDown) {
+      movePlayer(1, 0);
+      player.play('right');
+    } else if (cursors.up.isDown) {
+      movePlayer(0, -1);
+      player.play('up');
+    } else if (cursors.down.isDown) {
+      movePlayer(0, 1);
+      player.play('down');
+    } else if (autopilot_key.isDown) {
+      autoPilot();
+    } else {
+      if (!player.isMoving) {
+        player.animations.stop();
+      }
+    }
+
+  }
+
+  function render() {
+
+    game.debug.text('Level ' + dungeon.level, 16, 30);
+
+    game.debug.text('Use the ARROW KEYS to move', 16, game.height - 90);
+    game.debug.text('Press R to start a new game', 16, game.height - 60);
+    game.debug.text('Hold A for auto-pilot', 16, game.height - 30);
+
+  }
+});
