f864e110a64443783aa9ecfdb985f0c05ec0e8ab
Stephen Rumpel <jimbobmaggot@gmail.com>
2015-11-04 18:49:26 -0500

    More vm
    changed preload to use less 'this' as well

--- BETA/js/app/preload.js
+++ BETA/js/app/preload.js
@@ -9,13 +9,15 @@ define(['Phaser', 'game', 'dungeon', 'player'], function (Phaser, Game, dungeon,
         Preload = {
             // Import assets
             preload: function () {
+                // Used to avoid conflicts
+                var vm = this;
                 // TODO: Loading screen?
-                this.load.image('splash', 'assets/splash.png');
-                this.load.image('play', 'assets/play.png');
-                this.load.image('fullscreen', 'assets/fs.png');
-                this.load.image('dungeon', 'assets/Wall.png');
-                this.load.spritesheet('door', 'assets/Door.png', TILE_SIZE, TILE_SIZE);
-                this.load.spritesheet('door_open', 'assets/Door_Open.png', TILE_SIZE, TILE_SIZE);
+                vm.load.image('splash', 'assets/splash.png');
+                vm.load.image('play', 'assets/play.png');
+                vm.load.image('fullscreen', 'assets/fs.png');
+                vm.load.image('dungeon', 'assets/Wall.png');
+                vm.load.spritesheet('door', 'assets/Door.png', TILE_SIZE, TILE_SIZE);
+                vm.load.spritesheet('door_open', 'assets/Door_Open.png', TILE_SIZE, TILE_SIZE);
 
                 // Load player sprites
                 player.sprites.forEach(function (sprite) {
@@ -27,12 +29,12 @@ define(['Phaser', 'game', 'dungeon', 'player'], function (Phaser, Game, dungeon,
                 });
 
                 // Load Sound Effects
-                this.load.audio('SND_door_open', 'assets/sounds/Door.wav');
-                this.load.audio('SND_hit', 'assets/sounds/Hit.wav');
-                this.load.audio('SND_teleport', ['assets/sounds/Teleport.ogg', 'assets/sounds/Teleport.wav']);
+                vm.load.audio('SND_door_open', 'assets/sounds/Door.wav');
+                vm.load.audio('SND_hit', 'assets/sounds/Hit.wav');
+                vm.load.audio('SND_teleport', ['assets/sounds/Teleport.ogg', 'assets/sounds/Teleport.wav']);
                 // Load Music
-                this.load.audio('MUS_dungeon1', ['assets/music/Adventure_Meme.ogg', 'assets/music/Adventure_Meme.mp3']);
-                this.load.audio('MUS_dungeon2', ['assets/music/Wonderful_Nightmare.ogg', 'assets/music/Wonderful_Nightmare.mp3']);
+                vm.load.audio('MUS_dungeon1', ['assets/music/Adventure_Meme.ogg', 'assets/music/Adventure_Meme.mp3']);
+                vm.load.audio('MUS_dungeon2', ['assets/music/Wonderful_Nightmare.ogg', 'assets/music/Wonderful_Nightmare.mp3']);
             },
 
             create: function () {
